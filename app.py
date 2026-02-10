71e8d6c258f7c45ad2d1c5bd8c411eff502d4d37
dhrumilthakkar99 <dhrumilvthakkar@gmail.com>
2026-02-10 12:10:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.llms import HuggingFaceHub
-from langchain.chains import RetrievalQA
+from langchain.chains.retrieval_qa.base import RetrievalQA
 from PyPDF2 import PdfReader
 import os
 
